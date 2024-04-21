c89365ac974d62246a125ccc8971e5a4705a0f1c
Daniel Bertoldi <dsdanielbertoldi@gmail.com>
2024-04-21 16:53:21 -0300

    :test_tube: Dont know how to mock DataStore directly

--- app/src/test/java/daniel/bertoldi/geographyquiz/datasource/CountriesDefaultRemoteDataSourceTest.kt
+++ app/src/test/java/daniel/bertoldi/geographyquiz/datasource/CountriesDefaultRemoteDataSourceTest.kt
@@ -1,12 +1,16 @@
 package daniel.bertoldi.geographyquiz.datasource
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import daniel.bertoldi.geographyquiz.domain.mapper.BaseCountryDataResponseToModelMapper
 import daniel.bertoldi.geographyquiz.factory.BaseCountryDataResponseFactory
 import daniel.bertoldi.network.BaseCountryDataResponse
 import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.jupiter.api.Assertions
@@ -16,12 +20,10 @@ import retrofit2.Retrofit
 
 class CountriesDefaultRemoteDataSourceTest {
 
-    private val retrofit: Retrofit = mockk()
     private val countriesApi: CountriesApi = mockk()
     private val dataStore: DataStore<Preferences> = mockk()
     private val responseToModelMapper: BaseCountryDataResponseToModelMapper = mockk()
     private val remoteDataSource = CountriesDefaultRemoteDataSource(
-        retrofit = retrofit,
         countriesApi = countriesApi,
         dataStore = dataStore, // TODO: fix tests to actually mock this funny guy
         responseToModelMapper = responseToModelMapper,
@@ -57,9 +59,7 @@ class CountriesDefaultRemoteDataSourceTest {
     private fun prepareScenario(
         getCountriesResponse: Response<List<BaseCountryDataResponse>> = Response.success(emptyList())
     ) {
-        coEvery {
-            // TODO: this seems sketchy.
-            retrofit.create(CountriesApi::class.java).getCountries()
-        } returns getCountriesResponse
+        coEvery { countriesApi.getCountries() } returns getCountriesResponse
+        // coEvery { dataStore.updateData(transform = any()) } returns
     }
 }
